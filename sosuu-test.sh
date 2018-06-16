6a7323f81ac3bc324a74851a5cf05bb1ed642834
Ishikawa <a1805di@aiit.ac.jp>
2018-06-16 15:09:07 +0900

    sosuu Check

--- sosuu-test.sh
+++ sosuu-test.sh
@@ -4,33 +4,33 @@
 ./sosuu.sh 2 > /tmp/result-$$
 
 echo "2について判定します。" > /tmp/ans-$$
-echo "2は素数です。" > /tmp/ans-$$
+echo "2は素数です。" >> /tmp/ans-$$
 
-diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+diff /tmp/ans-$$ /tmp/result-$$ && exit 0
 
 
 ./sosuu.sh 3 > /tmp/result-$$
 
 echo "3について判定します。" > /tmp/ans-$$
-echo "3は素数です。" > /tmp/ans-$$
+echo "3は素数です。" >> /tmp/ans-$$
 
-diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+diff /tmp/ans-$$ /tmp/result-$$ && exit 0
 
 
 ./sosuu.sh 5 > /tmp/result-$$
 
 echo "5について判定します。" > /tmp/ans-$$
-echo "5は素数です。" > /tmp/ans-$$
+echo "5は素数です。" >> /tmp/ans-$$
 
-diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+diff /tmp/ans-$$ /tmp/result-$$ && exit 0
 
 
 ./sosuu.sh 997 > /tmp/result-$$
 
 echo "997について判定します。" > /tmp/ans-$$
-echo "997は素数です。" > /tmp/ans-$$
+echo "997は素数です。" >> /tmp/ans-$$
 
-diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+diff /tmp/ans-$$ /tmp/result-$$ && exit 0
 
 rm -f /tmp/$$-*
 
