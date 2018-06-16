1f55b347145c062f5b2efd23e4fdacb6dac225dd
Ishikawa <a1805di@aiit.ac.jp>
2018-06-16 13:40:55 +0900

    sosuutest

--- sosuu-test.sh
+++ sosuu-test.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-bash ./sosuu.sh
-./sosuu.sh > /tmp/result-$$
 
-echo "2は素数です。" > /tmp/ans-$$
+./sosuu.sh 2 >> /tmp/result-$$
+
+echo "2について判定します。" > /tmp/ans-$$
+echo "2は素数です。" >> /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+
+
+./sosuu.sh 3 >> /tmp/result-$$
+
+echo "3について判定します。" > /tmp/ans-$$
+echo "3は素数です。" >> /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+
+
+./sosuu.sh 5 >> /tmp/result-$$
+
+echo "5について判定します。" > /tmp/ans-$$
+echo "5は素数です。" >> /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
+
+
+./sosuu.sh 997 >> /tmp/result-$$
+
+echo "997"
+echo "997"
 
 diff /tmp/ans-$$ /tmp/result-$$ && echo "OK" exit 0
 
